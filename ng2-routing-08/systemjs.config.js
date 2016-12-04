bb982b948955928c54eabcc759361b04ad832a49
iproduct <office@iproduct.org>
2016-12-05 00:17:53 +0200

    base href and history api fallback for systemjs added

--- ng2-routing-08/systemjs.config.js
+++ ng2-routing-08/systemjs.config.js
@@ -4,14 +4,14 @@
  */
 (function (global) {
   System.config({
-     paths: {
+    paths: {
       // paths serve as alias
       'npm:': 'node_modules/'
     },
     // map tells the System loader where to look for things
     map: {
-      // our app is within the app folder
-      app: 'dist',
+      // our app is within the dist folder
+      'app': 'dist',
       // angular bundles
       '@angular/core': 'npm:@angular/core/bundles/core.umd.js',
       '@angular/common': 'npm:@angular/common/bundles/common.umd.js',
@@ -22,12 +22,12 @@
       '@angular/router': 'npm:@angular/router/bundles/router.umd.js',
       '@angular/forms': 'npm:@angular/forms/bundles/forms.umd.js',
       // other libraries
-      'rxjs':                      'npm:rxjs',
+      'rxjs': 'npm:rxjs',
       'angular-in-memory-web-api': 'npm:angular-in-memory-web-api',
     },
     // packages tells the System loader how to load when no filename and/or no extension
     packages: {
-      app: {
+      'app': {
         main: './main.js',
         defaultExtension: 'js'
       },
@@ -40,5 +40,61 @@
       }
     }
   });
-})(this);
 
+  // Stub out `app/main.ts` so System.import('app') doesn't fail if called in the index.html
+  // System.set(System.normalizeSync('app/main.ts'), System.newModule({}));
+
+
+  // bootstrap();
+
+  // Bootstrap with a default `AppModule`
+  // ignore an `app/app.module.ts` and `app/main.ts`, even if present
+  // This function exists primarily (exclusively?) for the QuickStart
+  function bootstrap() {
+    console.log('Auto-bootstrapping');
+
+    // Stub out `app/main.ts` so System.import('app') doesn't fail if called in the index.html
+    System.set(System.normalizeSync('app/main.ts'), System.newModule({}));
+
+    // bootstrap and launch the app (equivalent to standard main.ts)
+    Promise.all([
+      System.import('@angular/platform-browser-dynamic'),
+      getAppModule()
+    ])
+      .then(function (imports) {
+        var platform = imports[0];
+        var app = imports[1];
+        platform.platformBrowserDynamic().bootstrapModule(app.AppModule);
+      })
+      .catch(function (err) { console.error(err); });
+  }
+
+  // Make the default AppModule
+  // returns a promise for the AppModule
+  function getAppModule() {
+    console.log('Making a bare-bones, default AppModule');
+
+    return Promise.all([
+      System.import('@angular/core'),
+      System.import('@angular/platform-browser'),
+      System.import('app/app.component')
+    ])
+      .then(function (imports) {
+
+        var core = imports[0];
+        var browser = imports[1];
+        var appComp = imports[2].AppComponent;
+
+        var AppModule = function () { }
+
+        AppModule.annotations = [
+          new core.NgModule({
+            imports: [browser.BrowserModule],
+            declarations: [appComp],
+            bootstrap: [appComp]
+          })
+        ]
+        return { AppModule: AppModule };
+      })
+  }
+})(this);
