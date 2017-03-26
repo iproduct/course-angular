48c99a8e103f2e271f085401e0a5818289451ff5
iproduct <office@iproduct.org>
2017-03-26 12:38:14 +0300

    run tsc concurrently

--- 02-ts-demo-lab/app/index.ts
+++ 02-ts-demo-lab/app/index.ts
@@ -1,5 +1,5 @@
 export default function greeter(person: string) {
-    return 'Hello, ' + person + ' from Typescript and Angular!';
+    return 'Hello, ' + person + ' from Typescript and Angular 2!';
 }
 let user = 'TypeScript User';
 document.querySelector('#content').innerHTML = greeter(user);
