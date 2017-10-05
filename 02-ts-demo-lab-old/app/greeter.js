ae4eceab91cd4af88fb4e02a5bfee203c0fc4c6d
Trayan Iliev <office@iproduct.org>
2017-10-05 13:59:27 +0300

    02-ts-demo-lab renamed to 02-ts-demo-lab-old

--- 02-ts-demo-lab-old/app/greeter.js
+++ 02-ts-demo-lab-old/app/greeter.js
@@ -1,6 +1,5 @@
-'use strict';
 function greeter(person) {
     return 'Hello, ' + person + ' from Typescript and Angular!';
 }
 var user = 'TypeScript User';
-document.body.innerHTML = greeter(user);
+document.body.innerHTML = `<h2>${greeter(user)}</h2>`;
