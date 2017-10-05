b976cd6592f7a796cfd0d3d72fc641774e482f1d
Trayan Iliev <office@iproduct.org>
2017-10-05 12:59:25 +0300

    comments edited

--- 00-es6-features/async-await-github-demo.js
+++ 00-es6-features/async-await-github-demo.js
@@ -1,20 +1,20 @@
 async function showGithubData() {
-  // read our JSON
+  // fetch user name
   let response1 = await fetch("/user.json");
   let user = await response1.json();
   console.log(user);
 
-  // read github user
+  // fetch github user data
   let response2 = await fetch(`https://api.github.com/users/${user.name}`);
   let githubData = await response2.json();
   console.log(githubData);
 
-  // show the avatar
+  // show user picture
   let img = document.createElement("img");
   img.src = githubData.avatar_url;
   document.body.append(img);
 
-  // Hide after 5 seconds
+  // hide it after 5 seconds
   await new Promise((resolve, reject) => setTimeout(resolve, 5000));
 
   img.remove();
