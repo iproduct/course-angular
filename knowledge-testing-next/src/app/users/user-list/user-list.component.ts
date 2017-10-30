c7d2c36eeba90f0d5c3d509bbc383651e568b0a5
Trayan Iliev <ilievt@gmail.com>
2017-10-30 14:25:43 +0200

    routing animation added

--- knowledge-testing-next/src/app/users/user-list/user-list.component.ts
+++ knowledge-testing-next/src/app/users/user-list/user-list.component.ts
@@ -1,14 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostBinding } from '@angular/core';
 import { UserService } from '../user.service';
 import { User } from '../user.model';
 import { IdentityType, ApplicationError } from '../../shared/shared-types';
+import { slideInDownAnimation } from '../../shared/animations';
 
 @Component({
   selector: 'kt-user-list',
   templateUrl: './user-list.component.html',
-  styleUrls: ['./user-list.component.css']
+  styleUrls: ['./user-list.component.css'],
+  animations: [slideInDownAnimation]
 })
 export class UserListComponent implements OnInit {
+  @HostBinding('@routeAnimation') routeAnimation = true;
+  @HostBinding('style.display') display = 'block';
+  @HostBinding('style.width') width = '100%';
+  @HostBinding('style.position') position = 'absolute';
 
   users: User[] = [];
   selectedUser: User;
