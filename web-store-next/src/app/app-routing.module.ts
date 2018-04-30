0ec04ff2ddd20b885051283c8a5379f9232ba1c6
Trayan Iliev <ilievt@gmail.com>
2018-04-30 21:14:43 +0300

    License header explicitely  added to each source file.

--- web-store-next/src/app/app-routing.module.ts
+++ web-store-next/src/app/app-routing.module.ts
@@ -6,7 +6,30 @@ import { UserListComponent } from './users/user-list.component';
 import { SalesTaxComponent } from './sales/sales-tax.component';
 import { SimpleFormComponent } from './small-demos/simple.form.component';
 
-@NgModule({
+/*
+ * Copyright (c) 2015-2018 IPT-Intellectual Products & Technologies (IPT).
+ * All rights reserved.
+ * 
+ * This software provided by IPT-Intellectual Products & Technologies (IPT) is for 
+ * non-commercial illustartive and evaluation purposes only. 
+ * It is NOT SUITABLE FOR PRODUCTION purposes because it is not finished,
+ * and contains security flаws and weaknesses (like sending the passwords and 
+ * emails of users to the browser client, wich YOU SHOULD NEVER DO with real user
+ * data). You should NEVER USE THIS SOFTWARE with real user data.
+ * 
+ * This file is licensed under terms of GNU GENERAL PUBLIC LICENSE Version 3
+ * (GPL v3). The full text of GPL v3 license is providded in file named LICENSE,
+ * residing in the root folder of this repository.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * IPT BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+ @NgModule({
   imports: [
     RouterModule.forRoot([
       { path: '', redirectTo: '/home', pathMatch: 'full' },
