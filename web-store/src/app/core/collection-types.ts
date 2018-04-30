c58e61ca3008c488baca9cca135b0a3a5647e2ec
Trayan Iliev <ilievt@gmail.com>
2018-04-30 23:39:20 +0300

    projects renaming and cleaning

--- web-store/src/app/core/collection-types.ts
+++
@@ -1,5 +0,0 @@
-import { Identifiable } from '../shared/common-types';
-import { Product } from '../products/product.model';
-import { Type } from '@angular/core';
-
-export const COLLECTION_TYPES: Array<Type<Identifiable>> = [Product];
