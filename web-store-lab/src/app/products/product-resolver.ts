66dbb8089dad98827dac33da9dae0b0b01231d72
Trayan Iliev <ilievt@gmail.com>
2018-04-30 21:55:36 +0300

    product resolver handling the case when productId not found.

--- web-store-lab/src/app/products/product-resolver.ts
+++ web-store-lab/src/app/products/product-resolver.ts
@@ -3,6 +3,8 @@ import { Router, Resolve, ActivatedRouteSnapshot } from '@angular/router';
 
 import { Product } from './product.model';
 import { ProductsService } from './products.service';
+import { take, map, catchError } from 'rxjs/operators';
+import { ErrorObservable } from 'rxjs/observable/ErrorObservable';
 
 @Injectable()
 export class ProductResolver implements Resolve<Product> {
@@ -10,6 +12,20 @@ export class ProductResolver implements Resolve<Product> {
 
   public resolve(route: ActivatedRouteSnapshot) {
     const id = route.params['productId'];
-    return this.service.find(id);
+    return this.service.find(id).pipe(
+      take(1),
+      map(product => {
+        if (product) {
+          return product;
+        } else { // id not found
+            this.router.navigate(['/products']);
+            return null;
+        }
+      }),
+      catchError(err => {
+        this.router.navigate(['/products']);
+        return new ErrorObservable(`Product with ID:${id} not found.`);
+      })
+    );
   }
 }
