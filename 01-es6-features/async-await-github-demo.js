826467c9e61bb6f9dd3eb122b57a978e9660a9d7
Trayan Iliev <ilievt@gmail.com>
2018-06-06 14:47:47 +0300

    todo lab 3 added

--- 01-es6-features/async-await-github-demo.js
+++ 01-es6-features/async-await-github-demo.js
@@ -1,26 +1,30 @@
 async function showGithubData() {
-  // fetch user name
-  let response1 = await fetch("/user.json");
-  let user = await response1.json();
-  console.log(user);
+  try {
+    // fetch user name
+    let response1 = await fetch("/user.json");
+    let user = await response1.json();
+    console.log(user);
 
-  // fetch github user data
-  let response2 = await fetch(`https://api.github.com/users/${user.name}`);
-  let githubData = await response2.json();
-  console.log(githubData);
+    // fetch github user data
+    let response2 = await fetch(`https://api.github.com/users/${user.name}`);
+    let githubData = await response2.json();
+    console.log(githubData);
 
-  // show user picture
-  let img = new Image();
-  img.src = githubData.avatar_url;
-  document.body.append(img);
+    // show user picture
+    let img = new Image();
+    img.src = githubData.avatar_url;
+    document.body.append(img);
 
-  // hide it after 5 seconds
-  await new Promise((resolve, reject) => setTimeout(resolve, 5000));
+    // // hide it after 5 seconds
+    await new Promise((resolve, reject) => setTimeout(resolve, 5000));
 
-  img.remove();
-  console.log("Demo finished.")
+    img.remove();
+    console.log("Demo finished.");
+    return githubData;
 
-  return githubData;
+  } catch(err) {
+    console.log('ERROR: '  + err)
+  }
 }
 
-console.log(showGithubData());
+showGithubData().then(result => console.log(result));
