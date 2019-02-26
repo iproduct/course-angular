a9ffa1f3c7d245d2333f42fc4e0d953332f72e34
Trayan Iliev <office@iproduct.org>
2019-02-26 10:53:55 +0200

    programmatic validation added

--- 05-webstore-lab5/src/app/products/product-detail/product-detail.component.ts
+++ 05-webstore-lab5/src/app/products/product-detail/product-detail.component.ts
@@ -1,5 +1,16 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild } from '@angular/core';
 import { Product } from '../product.model';
+import { ErrorStateMatcher } from '@angular/material/core';
+import { FormControl, NgForm, AbstractControl } from '@angular/forms';
+import { formArrayNameProvider } from '@angular/forms/src/directives/reactive_directives/form_group_name';
+
+/** Error when invalid control is dirty, touched, or submitted. */
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: AbstractControl | null, form: NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
 
 @Component({
   selector: 'ws-product-detail',
@@ -9,10 +20,44 @@ import { Product } from '../product.model';
 export class ProductDetailComponent implements OnInit {
   @Input() mode = 'present';
   @Input() product: Product = new Product('', 0, '');
+  @ViewChild(NgForm) form: NgForm;
+
+  matcher = new MyErrorStateMatcher();
+
+  formErrors = {
+    name: ''
+  };
+
+  validationMessages = {
+    name: {
+      required: 'Product name is required.',
+      // 'minlength': 'Username must be at least 2 characters long.',
+      // 'maxlength': 'Username cannot be more than 24 characters long.'
+    }
+  };
 
   constructor() { }
 
   ngOnInit() {
   }
+
+  private onStatusChanged(data?: any) {
+    if (!this.form) { return; }
+    const form = this.form.form;
+
+    for (const field in this.formErrors) {
+      // clear previous error message (if any)
+      this.formErrors[field] = '';
+      const control = form.get(field);
+
+      if (control && control.dirty && !control.valid) {
+        const messages = this.validationMessages[field];
+        for (const key in control.errors) {
+          this.formErrors[field] += messages[key] + ' ';
+        }
+      }
+    }
+  }
+
 
 }
