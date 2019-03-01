c1e6d55e2375d3095a274aa2de02f37ee724aa9c
Trayan Iliev <office@iproduct.org>
2019-03-01 16:02:31 +0200

    product promise service completed

--- webstore-next/src/app/core/backend-http-promise.service.ts
+++ webstore-next/src/app/core/backend-http-promise.service.ts
@@ -1,14 +1,12 @@
 import { Injectable, Inject, forwardRef } from '@angular/core';
 import { Identifiable, ResourseType, IdType } from '../shared/shared-types';
 import { environment } from '../../environments/environment';
-import { map, tap } from 'rxjs/operators';
-import { HttpClient } from '@angular/common/http';
+import { map, tap, catchError, retry } from 'rxjs/operators';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { retryAfter } from '../shared/rx-operators';
+import { throwError } from 'rxjs';
 
-export interface CollectionResponse<T> {
-  data: T[];
-}
-
-export interface IndividualResponse<T> {
+export interface ServiceResponse<T> {
   data: T;
 }
 
@@ -21,39 +19,67 @@ export class BackendHtpPromiseService {
 
   find<T extends Identifiable> (kind: ResourseType<T>): Promise<T[]> {
     const collectionPath = this.getCollectionPath(kind.typeId);
-    return this.http.get<CollectionResponse<T>>(`${this.apiBaseUrl}/${collectionPath}`)
+    return this.http.get<ServiceResponse<T[]>>(`${this.apiBaseUrl}/${collectionPath}`)
       .pipe(
         map(resp => resp.data),
-        tap(data => console.log(data))
+        tap(data => console.log(data)),
+        retryAfter(2, 1000),
+        catchError(this.handleError)
       ).toPromise();
   }
 
-  // add<T extends Identifiable> (kind: ResourseType<T>, entity: T): Promise<T> {
-  //   entity.id = BackendHtpPromiseService.nextId++ + '';
-  //   const collection = this.getCollection(kind.typeId);
-  //   collection.push(entity);
-  //   return Promise.resolve(entity as T);
-  // }
+  add<T extends Identifiable> (kind: ResourseType<T>, entity: T): Promise<T> {
+    const collectionPath = this.getCollectionPath(kind.typeId);
+    return this.http.post<ServiceResponse<T>>(`${this.apiBaseUrl}/${collectionPath}`, entity)
+      .pipe(
+        map(resp => resp.data),
+        tap(data => console.log(`Successfully created ${kind.typeId}: ${JSON.stringify(data)}`)),
+        catchError(this.handleError)
+      ).toPromise();
+  }
 
-  // update<T extends Identifiable> (kind: ResourseType<T>, entity: T): Promise<T> {
-  //   const collection = this.getCollection(kind.typeId);
-  //   const index = collection.findIndex(e => e.id === entity.id);
-  //   collection[index] = entity;
-  //   return Promise.resolve(entity as T);
-  // }
+  update<T extends Identifiable> (kind: ResourseType<T>, entity: T): Promise<T> {
+    const collectionPath = this.getCollectionPath(kind.typeId);
+    return this.http.put<ServiceResponse<T>>(`${this.apiBaseUrl}/${collectionPath}/${entity.id}`, entity)
+    .pipe(
+      map(resp => resp.data),
+      tap(data => console.log(`Successfully updated ${kind.typeId}: ${JSON.stringify(data)}`)),
+      retryAfter(2, 1000),
+      catchError(this.handleError)
+    ).toPromise();
+  }
 
-  // delete<T extends Identifiable> (kind: ResourseType<T>, id: IdType): Promise<T> {
-  //   const collection = this.getCollection(kind.typeId);
-  //   const index = collection.findIndex(e => e.id === id);
-  //   const entity = collection.splice(index, 1)[0];
-  //   return Promise.resolve(entity as T);
-  // }
+  delete<T extends Identifiable> (kind: ResourseType<T>, id: IdType): Promise<T> {
+    const collectionPath = this.getCollectionPath(kind.typeId);
+    return this.http.delete<ServiceResponse<T>>(`${this.apiBaseUrl}/${collectionPath}/${id}`)
+    .pipe(
+      map(resp => resp.data),
+      tap(data => console.log(`Successfully updated ${kind.typeId}: ${JSON.stringify(data)}`)),
+      retryAfter(2, 1000),
+      catchError(this.handleError)
+    ).toPromise();
+  }
 
-  getCollectionPath(collectionName): string {
+  private getCollectionPath(collectionName): string {
     switch (collectionName) {
       case 'Product': return 'products';
       case 'User': return 'users';
     }
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error.message);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong,
+      console.error(
+        `Backend returned code ${error.status}, ` +
+        `body was: ${error.error}`);
+    }
+    // return an observable with a user-facing error message
+    return throwError('There was a problem with backend service. Try again later.');
   }
 
 }
