fc5472fc83a3d3e93e37267b1e709cfd770299dc
Trayan Iliev <office@iproduct.org>
2019-10-17 16:56:17 +0300

    base routing added

--- 05-webstore-lab8/src/app/app-routing.module.ts
+++ 05-webstore-lab8/src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProductsListComponent } from './products/products-list/products-list.component';
+import { RxDemoComponent } from './rx-demo/rx-demo/rx-demo.component';
+import { WikiComponent } from './wiki/wiki.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/products', pathMatch: 'full'},
+  {path: 'products', component: ProductsListComponent},
+  {path: 'rxdemo', component: RxDemoComponent},
+  {path: 'wiki', component: WikiComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
